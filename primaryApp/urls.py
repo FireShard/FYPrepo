d8bb5f6ae826d97f09fc83e211ad53219187cc10
FireShard <fakhrulnaim4321@gmail.com>
2025-01-21 23:22:30 +0800

    test2

--- primaryApp/urls.py
+++ primaryApp/urls.py
@@ -7,9 +7,5 @@ router.register(r'UserLaptop', views.UserLaptopViewSet)
 router.register(r'LaptopDetails', views.LaptopDetailsViewSet)
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', include(router.urls)),
-=======
-    path('', include(router.urls))
->>>>>>> 747fc1f3b5d1ae841d7631ca7dba34fc42cbe430
 ]
